2b497b1f9598db3b7238043d12e72407d396698d
knebao <116298744+knebao@users.noreply.github.com>
2026-01-22 01:54:35 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+﻿const CONFIG = {
+    introTitle: 'Bbi à!',
+    introDesc: `Khi em nằm bên anh
+    Xem từng hạt mưa rơi`,
+    btnIntro: 'Bấm tiếp đi cục cưng',
+    title: 'Em có yêu anh hong vợ',
+    desc: 'Trả lời nghe vừa tai thì thưởng cho',
+    btnYes: 'Em yêu chồng nhiềuuu',
+    btnNo: 'Nhìn mặt thấy ghét ai mà yêu',
+    question: 'Tại sao em yêu anh dọo',
+    btnReply: 'Send',
+    reply: 'Tại anh đẹp trai óoo',
+    mess: 'Anh không đẹp thì ai đẹp nữa',
+    messDesc: 'Tí lên anh thưởng cho cây chúc chích nhó',
+    btnAccept: 'Dạaa',
+    messLink: 'https://m.me/knebao' //link mess của các bạn. VD: 
+}
